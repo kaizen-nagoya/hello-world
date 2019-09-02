b248277d1eb0a4252451c34fd20ec95842b95060
kaizen_nagoya <kaizen@wh.commufa.jp>
2019-09-02 12:49:59 +0900

    Create gccl.sh

---
+++ gccl.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# (c) @kaizen_nagoya
+echo "https://qiita.com/kaizen_nagoya/items/79e771ff681fb27f6070"
+echo "$ clang $1.c $2  $3 -o $1l"
+clang $1.c $2 $3 -o $1l 
+if [  -e $1l ]; then
+./$1l 
+fi
+echo "\r"
+echo "$ gcc $1.c $2 $3 -o $1g"
+gcc $1.c $2 $3 -o $1g  
+if [  -e $1g ]; then
+./$1g 
+fi
+$4 $6
+$5 $6
