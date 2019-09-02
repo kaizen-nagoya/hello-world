9ec3bf7c9fbedb3b397de395f86f7005fce39e08
kaizen_nagoya <kaizen@wh.commufa.jp>
2019-09-02 14:37:11 +0900

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,10 @@
+//https://qiita.com/qnighy/items/be04cfe57f8874121e76
+#include <stdio.h>
+int main(int argc, char ** argv)
+{
+	if (argc >2 ) {
+    		return printf("%s %s\n", argv[1], argv[2] );
+	} else {
+		return printf("little argument %d %s needs 2 parameters\n", argc, argv[0]);
+	}
+}
