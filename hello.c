a9c15625ae48f4e5f2bf330a479c0319033a8071
kaizen_nagoya <kaizen@wh.commufa.jp>
2019-09-02 15:03:44 +0900

    adjust 2nd printf messages.

--- hello.c
+++ hello.c
@@ -5,6 +5,6 @@ int main(int argc, char ** argv)
 	if (argc >2 ) {
     		return printf("%s %s\n", argv[1], argv[2] );
 	} else {
-		return printf("little argument %d. %s needs 2 parameters\n", argc-1, argv[0]);
+		return printf("Little parameters %d. %s needs 2 parameters.\n", argc-1, argv[0]);
 	}
 }
