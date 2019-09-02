5884cecead261eaa65ebe19e68d295f50c0902c4
kaizen_nagoya <kaizen@wh.commufa.jp>
2019-09-02 15:00:09 +0900

    calculate argument counter
    subtracting command name

--- hello.c
+++ hello.c
@@ -5,6 +5,6 @@ int main(int argc, char ** argv)
 	if (argc >2 ) {
     		return printf("%s %s\n", argv[1], argv[2] );
 	} else {
-		return printf("little argument %d. %s needs 2 parameters\n", argc, argv[0]);
+		return printf("little argument %d. %s needs 2 parameters\n", argc-1, argv[0]);
 	}
 }
